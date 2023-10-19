d54e4eb77e84238d3513a7c48256cbe32915df33
levdalba <levanidalbashvili00@gmail.com>
2023-10-19 11:04:56 +0700

    added ui and its working

--- board.py
+++ board.py
@@ -2,39 +2,54 @@ class Board:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.board = [[False] * width for i in range(height)]
+        self.cells = [[False for _ in range(width)] for _ in range(height)]
 
-    def __str__(self):
-        rv = ""
-        for row in self.board:
-            rv += "".join(".0"[int(C)] for C in row) + "\n"
-        return rv
+    def is_alive(self, row, col):
+        if row < 0 or row >= self.height or col < 0 or col >= self.width:
+            return False
+        return self.cells[row][col]
 
     def place_cell(self, row, col):
-        self.board[row][col] = True
+        if row < 0 or row >= self.height or col < 0 or col >= self.width:
+            return
+        self.cells[row][col] = True
+
+    def remove_cell(self, row, col):
+        if row < 0 or row >= self.height or col < 0 or col >= self.width:
+            return
+        self.cells[row][col] = False
+
+    def toggle_cell(self, row, col):
+        if self.is_alive(row, col):
+            self.remove_cell(row, col)
+        else:
+            self.place_cell(row, col)
+
+    def count_neighbors(self, row, col):
+        count = 0
+        for i in range(row - 1, row + 2):
+            for j in range(col - 1, col + 2):
+                if i == row and j == col:
+                    continue
+                if self.is_alive(i, j):
+                    count += 1
+        return count
 
     def next(self):
-        self.new_board = [[False] * self.width for i in range(self.height)]
-        for row_num in range(self.width):
-            for col_num in range(self.height):
-                n = self.get_num_neighbor(row_num, col_num)
-                if not self.board[row_num][col_num]:
-                    if n == 3:
-                        self.new_board[row_num][col_num] = True
+        new_cells = [[False for _ in range(self.width)] for _ in range(self.height)]
+        for row in range(self.height):
+            for col in range(self.width):
+                count = self.count_neighbors(row, col)
+                if self.is_alive(row, col):
+                    if count == 2 or count == 3:
+                        new_cells[row][col] = True
                 else:
-                    if n == 2 or n == 3:
-                        self.new_board[row_num][col_num] = True
-        tmp = self.board
-        self.board = self.new_board
-        self.new_board = tmp
+                    if count == 3:
+                        new_cells[row][col] = True
+        self.cells = new_cells
 
-    def get_num_neighbor(self, row, col):
-        counter = 0
-        for r in (-1, 0, 1):
-            for c in (-1, 0, 1):
-                if not (r == 0 and c == 0):
-                    if 0 <= row + r < self.height and 0 <= col + c < self.width:
-                        if self.board[row + r][col + c]:
-                            counter += 1
-
-        return counter
+    def __str__(self):
+        return "\n".join(
+            "".join(".o"[self.cells[row][col]] for col in range(self.width))
+            for row in range(self.height)
+        )
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from board import Board
 import time
 
-board = Board(3, 3)
-board.place_cell(0, 1)
-board.place_cell(1, 1)
-board.place_cell(2, 1)
 
+board = Board(10, 10)
+board.place_cell(5, 2)
+board.place_cell(5, 3)
+board.place_cell(5, 4)
 
 while True:
     time.sleep(1)
